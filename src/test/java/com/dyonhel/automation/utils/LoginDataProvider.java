c638a4f75a7321c1b186250d8e3ab947bf4b7bb9
DyoNheL <dyonhel.github@gmail.com>
2026-01-21 10:18:28 +0800

    Update LoginDataProvider.java to include the login credentials and expected result

--- src/test/java/com/dyonhel/automation/utils/LoginDataProvider.java
+++ src/test/java/com/dyonhel/automation/utils/LoginDataProvider.java
@@ -1,5 +1,18 @@
 package com.dyonhel.automation.utils;
 
+import org.testng.annotations.DataProvider;
+
 public class LoginDataProvider {
+
+    @DataProvider(name = "loginData")
+    public Object[][] loginData() {
+        return new Object[][] {
+                {"standard_user", "secret_sauce", "success"},
+                {"locked_out_user", "secret_sauce", "failure"},
+                {"problem_user", "secret_sauce", "success"},
+                {"performance_glitch_user", "secret_sauce", "success"},
+                {"invalid_user", "invalid_pass", "failure"}
+        };
+    }
 
 }
